b5abc0eed1c26f066dc8e9c278b691f07061da85
Beardocracy <travisjbearden@gmail.com>
2020-04-06 23:26:05 +0000

    Added part 6

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# Install NGINX and configure it with puppet
+
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file_line { 'redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://youtu.be/WPkMUU9tUqk permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+service { 'start nginx':
+  ensure => running,
+  require => Package['nginx'],
+}
