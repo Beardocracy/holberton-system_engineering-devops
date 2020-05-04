31443d5beb57c027620d1ea0e316a9e383e68d82
Beardocracy <travisjbearden@gmail.com>
2020-05-04 23:08:11 +0000

    Fixed pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     if sys.argv[1]:
         user_id = sys.argv[1]
         if int(user_id) > 10 or int(user_id) < 1:
-            exit (0)
+            exit(0)
 
     user_response = requests.get(user_info_url + user_id)
     todos_response = requests.get(todo_url + user_id)
