b66341542512f825f18eb7fc7011b27b925b3866
Beardocracy <travisjbearden@gmail.com>
2020-05-04 23:27:36 +0000

    Added import guard

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 ''' Exports data from API in JSON '''
+import json
 import requests
 import sys
-import json
 
+if __name__ == "__main__":
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+    user_info_url = 'https://jsonplaceholder.typicode.com/users/'
 
-todo_url = 'https://jsonplaceholder.typicode.com/todos?userId='
-user_info_url = 'https://jsonplaceholder.typicode.com/users/'
+    users_list = requests.get(user_info_url).json()
 
-users_list = requests.get(user_info_url).json()
+    data = {}
+    for user in users_list:
+        user_id = user['id']
+        todo_resp = requests.get(todo_url + str(user_id))
+        user_todos = todo_resp.json()
 
-data = {}
-for user in users_list:
-    user_id = user['id']
-    todo_resp = requests.get(todo_url + str(user_id))
-    user_todos = todo_resp.json()
+        task_data = [{
+            "task":         todo['title'],
+            "completed":    todo['completed'],
+            "username":     user['username']
+        } for todo in user_todos]
 
-    task_data = [{
-        "task":         todo['title'],
-        "completed":    todo['completed'],
-        "username":     user['username']
-    } for todo in user_todos]
+        data[user_id] = task_data
 
-    data[user_id] = task_data
-
-
-with open('todo_all_employees.json', mode='w') as f:
-    json.dump(data, f)
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(data, f)
