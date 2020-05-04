0e2a457d0defd9cf2fcf6971534ae9b246564d9a
Beardocracy <travisjbearden@gmail.com>
2020-05-04 23:13:56 +0000

    Added import guard

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,29 +4,30 @@ import requests
 import sys
 import csv
 
+if __name__ == "__main__":
+    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId='
+    user_info_url = 'https://jsonplaceholder.typicode.com/users/'
 
-todo_url = 'https://jsonplaceholder.typicode.com/todos?userId='
-user_info_url = 'https://jsonplaceholder.typicode.com/users/'
+    if sys.argv[1]:
+        user_id = sys.argv[1]
+        if int(user_id) > 10 or int(user_id) < 1:
+            print("Employee doesn't exist")
+            exit(0)
 
-if sys.argv[1]:
-    user_id = sys.argv[1]
-    if int(user_id) > 10 or int(user_id) < 1:
-        print("Employee doesn't exist")
-        exit(0)
+    user_response = requests.get(user_info_url + user_id)
+    todos_response = requests.get(todo_url + user_id)
 
-user_response = requests.get(user_info_url + user_id)
-todos_response = requests.get(todo_url + user_id)
+    user_data = user_response.json()
+    user_todos = todos_response.json()
 
-user_data = user_response.json()
-user_todos = todos_response.json()
+    with open('{}.csv'.format(user_id), mode='w') as f:
+        writer = csv.writer(f, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
 
-with open('{}.csv'.format(user_id), mode='w') as f:
-    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-
-    for todo in user_todos:
-        writer.writerow([
-            user_id,
-            user_data['name'],
-            todo['completed'],
-            todo['title']
-        ])
+        for todo in user_todos:
+            writer.writerow([
+                user_id,
+                user_data['name'],
+                todo['completed'],
+                todo['title']
+            ])
