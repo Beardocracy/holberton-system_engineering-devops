a7d43e24c23e79c00e485aff6f44a3f560910733
Beardocracy <travisjbearden@gmail.com>
2020-05-07 20:51:23 +0000

    Chmodded

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,10 +5,11 @@ import requests
 
 def top_ten(subreddit):
     ''' Prints top ten hot posts for a subreddit '''
-    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-agent': 'tbearden'}
+    params = {'limit': 10}
 
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, params=params)
     data = r.json()
 
     children = data['data']['children']
@@ -16,5 +17,5 @@ def top_ten(subreddit):
         print("None")
     else:
         headlines = [child['data']['title'] for child in children]
-        for i in range(10):
-            print(headlines[i])
+        for titles in headlines:
+            print(titles)
